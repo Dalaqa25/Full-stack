9b8174febfbb895f2d4362d95190ebd7c14db858
Dalaqa25 <g.dalaqishvili01@gmail.com>
2024-10-29 09:58:11 +0400

    sign in/up done

--- testing/script.js
+++
@@ -1,9 +0,0 @@
-let position = 0; // Current position of section-1
-const maxPosition = 250; // Maximum position to move within container
-
-function moveRight() {
-  if (position < maxPosition) {
-    position += 250; // Adjust the step size for each click
-    document.getElementById('section1').style.left = position + 'px';
-  }
-}
