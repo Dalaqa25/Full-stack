8a3b2e90a27718c4ba9d79b26d9ed8ea7e5c528c
Dalaqa25 <g.dalaqishvili01@gmail.com>
2024-12-02 21:46:39 +0400

    SDDI change

--- Syncro/Main.js
+++ Syncro/Main.js
@@ -2,14 +2,17 @@ const profileBtn = document.getElementById("profileIcon");
 const homeBtn = document.getElementById("homeIcon");
 const projectBtn = document.getElementById("folderIcon");
 const contentPage = document.getElementById("contentPage");
+const welcomePage = document.getElementById("welcomePage");
+const profilePgae = document.getElementById("profilePgae");
 
 homeBtn.addEventListener("click", () => {
     contentPage.style.backgroundColor = "green";
 })
 
-profileBtn.addEventListener("click", () => {
-    contentPage.style.backgroundColor = "purple";
-})
+profileBtn.addEventListener("click", () => {    
+    welcomePage.style.display = "none";
+    profilePgae.style.display = "block";
+});
 
 projectBtn.addEventListener("click", () => {
     contentPage.style.backgroundColor = "red";
