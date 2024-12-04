129774b7f72f7ab54e0a828f1261698b94fed313
Dalaqa25 <g.dalaqishvili01@gmail.com>
2024-12-04 21:35:08 +0400

    menu bar optimization

--- Syncro/Main.js
+++ Syncro/Main.js
@@ -4,9 +4,26 @@ const projectBtn = document.getElementById("folderIcon");
 const contentPage = document.getElementById("contentPage");
 const welcomePage = document.getElementById("welcomePage");
 const profilePage = document.getElementById("profilePage");
+const navbarMobile = document.getElementById("navbarMobile");
+const menuBtn = document.getElementById("menuBtn");
+const closeBtn = document.getElementById("closeBtn");
 
-
-profileBtn.addEventListener("click", () => {
+profileBtn.addEventListener("click", () => 
+{
     profilePage.style.display = "block";
     welcomePage.style.display = "none";    
-})
+});
+
+menuBtn.addEventListener("click", () => 
+{
+    menuBtn.style.display = "none";
+    navbarMobile.style.display = "block";
+    closeBtn.style.display = "block";
+});
+
+closeBtn.addEventListener("click", () => 
+{
+    navbarMobile.style.display = "none";
+    menuBtn.style.display = "block";
+    closeBtn.style.display = "none";
+});
