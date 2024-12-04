3dfa66e1f179c556e28bcce45f8cfe6a38862f26
Dalaqa25 <g.dalaqishvili01@gmail.com>
2024-12-04 18:57:48 +0400

    CDI changed

--- Syncro/Main.js
+++ Syncro/Main.js
@@ -3,7 +3,7 @@ const homeBtn = document.getElementById("homeIcon");
 const projectBtn = document.getElementById("folderIcon");
 const contentPage = document.getElementById("contentPage");
 const welcomePage = document.getElementById("welcomePage");
-const profilePage = document.getElementById("profilePgae");
+const profilePage = document.getElementById("profilePage");
 
 
 profileBtn.addEventListener("click", () => {
