bc7e1c49ceaf94d76794c0d7902e79d9526955e5
Dalaqa25 <g.dalaqishvili01@gmail.com>
2024-12-06 19:06:04 +0400

    dasd

--- Syncro/Main.js
+++ Syncro/Main.js
@@ -1,6 +1,3 @@
-const profileBtn = document.getElementById("profileIcon");
-const homeBtn = document.getElementById("homeIcon");
-const projectBtn = document.getElementById("folderIcon");
 const contentPage = document.getElementById("contentPage");
 const welcomePage = document.getElementById("welcomePage");
 const profilePage = document.getElementById("profilePage");
@@ -8,10 +5,14 @@ const navbarMobile = document.getElementById("navbarMobile");
 const menuBtn = document.getElementById("menuBtn");
 const closeBtn = document.getElementById("closeBtn");
 
-profileBtn.addEventListener("click", () => 
-{
-    profilePage.style.display = "block";
-    welcomePage.style.display = "none";    
+const profileIcon = document.querySelector(".profileIcon");
+profileIcon.addEventListener("click", () => {
+    welcomePage.style.display = "none";
+});
+
+const profileIconMobile = document.getElementById("profileIconMobile");
+profileIconMobile.addEventListener("click", () => {
+    welcomePage.style.display = "none";
 });
 
 menuBtn.addEventListener("click", () => 
